ec90d87e25391f77ded661d05e0ad46e98833434
Renan Ferrari <134348963+devrenanferrari@users.noreply.github.com>
2025-05-12 22:02:25 -0300

    Update analyze_code.py

--- .github/workflows/analyze_code.py
+++ .github/workflows/analyze_code.py
@@ -1,11 +1,14 @@
 import subprocess
 
 def analyze_pylint():
-    result = subprocess.run(['pylint', '--disable=R,C', '--output-format=text'], capture_output=True, text=True)
+    result = subprocess.run(
+        ['pylint', '.', '--disable=R,C', '--output-format=text'],
+        capture_output=True, text=True
+    )
     return result.stdout
 
 def analyze_flake8():
-    result = subprocess.run(['flake8', '--max-line-length=79'], capture_output=True, text=True)
+    result = subprocess.run(['flake8', '.'], capture_output=True, text=True)
     return result.stdout
 
 def analyze_bandit():
@@ -17,14 +20,13 @@ def main():
     flake8_result = analyze_flake8()
     bandit_result = analyze_bandit()
 
-    # Salvar os resultados em um arquivo de texto
-    with open("analysis_report.txt", "w") as file:
-        file.write("Resultado da análise com PyLint:\n")
-        file.write(pylint_result)
-        file.write("\nResultado da análise com Flake8:\n")
-        file.write(flake8_result)
-        file.write("\nResultado da análise de segurança com Bandit:\n")
-        file.write(bandit_result)
+    with open("analysis_report.txt", "w") as report_file:
+        report_file.write("### Pylint Report ###\n")
+        report_file.write(pylint_result + "\n")
+        report_file.write("### Flake8 Report ###\n")
+        report_file.write(flake8_result + "\n")
+        report_file.write("### Bandit Report ###\n")
+        report_file.write(bandit_result + "\n")
 
 if __name__ == "__main__":
     main()
